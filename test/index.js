fd4874872ec2b79e32cc9067413a290c8d3781ea
Adrien ASESIO <aasesio@gmail.com>
2020-03-12 12:38:19 -0400

    Better error handling

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ main()
 
 async function main() {
     try {
-        const video = await new ffmpeg('test.mov')
+        const video = await new ffmpeg('test2.mp4')
         console.log(video.metadata)
 
         console.log('Setting video codec')
@@ -12,10 +12,10 @@ async function main() {
         video.setVideoFormat('mp4')
 
         const rotated = [90, 180].includes(video.metadata.video.rotate)
-        if (video.metadata.video.codec == 'h264' && video.metadata.audio.bitrate < 2800 &&
+        if (video.metadata.video.codec == 'h264' && video.metadata.video.bitrate < 2800 &&
             video.metadata.video.fps <= 31 &&
-            ((rotated && video.metadata.video.resolution.w <= 720) ||
-                (!rotated && video.metadata.video.resolution.h <= 720))) {
+            ((rotated && video.metadata.video.resolution.w <= 1000) ||
+                (!rotated && video.metadata.video.resolution.h <= 1000))) {
             console.log('Video set to copy.')
             video.setVideoCodec('copy')
         } else {
@@ -41,14 +41,15 @@ async function main() {
         } else {
             video.setAudioCodec('aac')
             video.setAudioChannels(2)
-            video.setAudioBitrate('128k')
-            video.setAudioFrequency('44.1k')
+            video.setAudioBitRate(128)
+            video.setAudioFrequency(44100)
         }
 
         const result = await video.save('test-output.mp4')
         console.log('Final result:')
         console.log(result)
     } catch (e) {
+        console.log('Error Caught!')
         console.log(e)
     }
 }
